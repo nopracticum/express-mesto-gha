075379af496f59234c838999072ce5fb3b9654fe
Nikita Sinitsyn <n.o.sinitsyn@ya.ru>
2023-08-01 18:22:10 +0400

    14 sprint/1 review

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 const error = (err, req, res, next) => {
-  const { statusCode = 500 } = 500;
+  const { statusCode = 500 } = err;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
 
   res.status(statusCode).send({ message });
